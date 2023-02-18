339b45b04da4006b223de7ae7b7aa13e820afd74
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-18 17:22:15 +1100

    toggling around tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div >
-      <h1 className="text-3xl font-bold underline">HELOOOO</h1>
+    <div className=" bg-teal-800 flex justify-center" >
+      <h1 className=" text-white text-3xl font-bold underline">HELOOOO</h1>
 
     </div>
   );
